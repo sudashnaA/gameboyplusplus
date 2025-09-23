1b33da305e9f476b164c0799f62d6f2482f62b4f
SudashnaA <amarakoonsudashna@gmail.com>
2025-09-23 15:22:12 +1000

    initial commit

---
+++ CPU.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+
+class CPU
+{
+	struct Registers {
+		uint8_t a{};
+		uint8_t b{};
+		uint8_t c{};
+		uint8_t d{};
+		uint8_t e{};
+		uint8_t f{};
+		uint8_t g{};
+		uint8_t h{};
+		uint8_t l{};
+	};
+
+
+};
+
