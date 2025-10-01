411cd88ae2911c7388bc4ee39e73fa19dc23ecea
SudashnaA <amarakoonsudashna@gmail.com>
2025-10-01 20:26:02 +1000

    Created function to return instructions array

---
+++ Instruction.cpp
@@ -0,0 +1,21 @@
+#include "Instruction.h"
+#include <array>
+
+
+constexpr auto getInstructions()
+{
+	using enum InstructionType;
+	using enum ConditionType;
+	using enum RegisterType;
+	using enum AddressMode;
+
+	std::array<Instruction, 0x100> instructions{};
+	instructions[0x05] = { IN_NOP, AM_IMP };
+	instructions[0x05] = { IN_DEC, AM_R, RT_B };
+	instructions[0x0E] = { IN_LD, AM_R_D8, RT_C };
+	instructions[0xAF] = { IN_XOR, AM_R, RT_A };
+	instructions[0xC3] = { IN_JP, AM_D16 };
+	instructions[0xF3] = { IN_DI };
+
+	return instructions;
+}
